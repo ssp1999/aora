0e8541527b519dbea76bf2788cd8d743dfa52d99
shakyra <shakyra.portes@gmail.com>
2024-11-03 13:40:56 -0300

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Link } from 'expo-router'
+
+export default function App() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-3xl font-pblack'>aora</Text>
+      <StatusBar style='auto' />
+      <Link href='/profile' className='text-secondary'>go to profile</Link>
+    </View>
+  )
+}
